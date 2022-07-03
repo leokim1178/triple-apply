60f37c4f34704ebfb637c7b7180ec871597a8c8a
LeoKim <leo3179@naver.com>
2022-07-03 20:55:20 +0900

    🎉 INIT / 데이터베이스 설정 및 docker 환경 구축
    - DB 환경 설정
    - docker 환경 설정, 관련 파일 추가
    - TypeORM 연결
    - TypeORM 관련 모듈 추가
    
    Relate: [INIT] 프로젝트 보일러 플레이트 세팅 #1

--- triple-project/src/app.module.ts
+++ triple-project/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database-server',
+      port: 3306,
+      username: 'root',
+      password: '1178',
+      database: 'triple-database',
+      entities: [__dirname + '/apis/**/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+      retryAttempts: 30,
+      retryDelay: 5000,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
