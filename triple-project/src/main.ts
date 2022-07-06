29b8a1e6be0fca5989085de45a31e74581ba5a20
LeoKim <leo3179@naver.com>
2022-07-07 04:28:41 +0900

    ✨ 예외 처리 필터 생성
    - 전역 예외 처리 필터 생성
    - 전역으로 사용 설정
    
    Relate: #11

--- triple-project/src/main.ts
+++ triple-project/src/main.ts
@@ -1,12 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception-filter';
 
 import { BaseApiDocumentation } from './config/baseApiDocs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const documentOptions = new BaseApiDocumentation().initializeOptions();
   const document = SwaggerModule.createDocument(app, documentOptions);
